a52ea61ab77c105aad467d2fa33351018a26801d
Siddharth Surana <s2surana@edu.uwaterloo.ca>
2019-10-17 17:02:25 -0400

    integrate NEAT config

--- evo_sim.py
+++ evo_sim.py
@@ -3,6 +3,7 @@ import neat
 import time
 import os
 import random
+from scipy.spatial import distance
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 600
@@ -27,7 +28,7 @@ class Bot:
             self.img = pygame.transform.flip(self.BOT_IMG, True, False)
         else:
             self.img = self.BOT_IMG
-            
+
     def up(self):
         self.y -= self.vel
         self.direction = 0
@@ -72,6 +73,8 @@ class Food:
         return False
 
 def draw_window(win, bots, food):
+    win.fill((255,255,255))
+    
     for bot in bots:
         bot.draw(win)
 
@@ -80,7 +83,27 @@ def draw_window(win, bots, food):
 
     pygame.display.update()
 
-def main():
+def find_closest_food(curr_x, curr_y, food):
+    p1 = (curr_x, curr_y)
+    distance_to_food = []
+
+    for x, f in enumerate(food):
+        p2 = (f.x, f.y)
+        distance_to_food.append(distance.euclidean(p1, p2))
+    
+    return  distance_to_food.index(min(distance_to_food))
+
+def main(genomes, config):
+    nets = []
+    ge = []
+    #bots = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+        ge.append(g)
+
     bots_x_a = ([10] * 5)
     bots_x_b = ([740] * 5) 
     bots_y_a = random.sample(range(10, 550), 5)
@@ -105,23 +128,60 @@ def main():
 
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     win.fill((255,255,255))
+
+    clock = pygame.time.Clock()
+
     run = True
 
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
-        for bot in bots:
-            for f in food:
+                pygame.quit()
+                quit()
+
+        for x, bot in enumerate(bots):
+            index_closest_food = find_closest_food(bot.x, bot.y, food)
+            output = nets[bots.index(bot)].activate((bot.x, bot.y, food[index_closest_food].x, food[index_closest_food].y))
+            
+            if output[0] > 0.5:
+                bot.up()
+            if output[1] > 0.5:
+                bot.right()
+            if output[2] > 0.5:
+                bot.down()
+            if output[3] > 0.5:
+                bot.left()
+
+            print("output: ")
+            print(output)
+
+        for x, bot in enumerate(bots):
+            for y, f in enumerate(food):
                 if f.collide(bot):
-                    score += 1
+                    ge[x].fitness += 1
+                    food.pop(y)
 
         draw_window(win, bots, food)
-            
-
-    pygame.quit()
-    quit()
 
 
-main()
+#main()
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+    neat.DefaultSpeciesSet, neat.DefaultStagnation, 
+    config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 50)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-nn.txt")
+    run(config_path)
